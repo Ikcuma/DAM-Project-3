f55dfc53f9ae5b5b3a9129e61713415f3ae4990a
Hugo Cruz <dunkletodeus@gmail.com>
2025-03-19 12:28:23 +0100

    clases

---
+++ src/src/football_manager/MainTest.java
@@ -0,0 +1,28 @@
+package football_manager;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class MainTest {
+
+    @Test
+    void main() {
+    }
+
+    @Test
+    void transferPlayerOrCoach() {
+    }
+
+    @Test
+    void printTeamData() {
+    }
+
+    @Test
+    void capitalizeFirstLetterNames() {
+    }
+
+    @Test
+    void reewriteFileMarket() {
+    }
+}
